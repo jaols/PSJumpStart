f51716a6122c5d99f3264221e6f89ce9de593c43
Jack Olsson <jackgolsson@gmail.com>
2018-07-10 14:09:26 +0200

    Template retreival functions

--- PSJumpStart/PSJumpStart.psm1
+++ PSJumpStart/PSJumpStart.psm1
@@ -1,4 +1,65 @@
 #region Under-Construction and test code
+function Copy-PSTemplate {
+  <#
+    .SYNOPSIS
+        List template files
+    .DESCRIPTION
+        List available template files
+    .PARAMETER Name
+        Name of template(s) 
+    .PARAMETER 
+        Name of template(s) 
+#>  
+[CmdletBinding()]
+    Param(     
+	    [string]$Name,
+        [string]$Destination
+    )
+    Write-Verbose "Copy template(s) from $PSScriptRoot\Templates"
+
+    if ([string]::IsNullOrEmpty($Name)) {
+        Get-ChildItem "$PSScriptRoot\Templates" | Copy-Item -Destination $Destination
+    } else {
+        Get-ChildItem "$PSScriptRoot\Templates" -Filter "$Name" | Copy-Item -Destination $Destination
+    }
+}
+
+function Find-PSTemplate {
+<#
+.SYNOPSIS
+    List template files
+.DESCRIPTION
+    List available template files
+.PARAMETER Name
+    Name of template(s) 
+#>
+[CmdletBinding()]
+    Param(     
+	    [string]$Name
+    )
+    
+    Write-Verbose "Find templates in $PSScriptRoot\Templates"
+
+    if ([string]::IsNullOrEmpty($Name)) {
+        $list = Get-ChildItem "$PSScriptRoot\Templates"
+    } else {
+        $list = Get-ChildItem "$PSScriptRoot\Templates" -Filter "$Name"
+    }
+
+    ForEach($template in $list) {
+        $Description = ""
+        if ($template.Extension -ieq ".ps1") {
+            $Description = (Get-help $template.FullName -ShowWindow:$false).Description[0].Text
+        }
+        
+        $return = [PSCustomObject]@{
+            Name = $template.Name
+            Description = $Description
+        }
+
+        $return
+    }
+}
 
 function verboseTest {
 [CmdletBinding()]
@@ -29,70 +90,7 @@ function GatherErrorTest
 
     }
 }
-function ExportDataTableToFile {
-<#
-.SYNOPSIS
-    Dump a datatable to CSV-file OR XML-file
-.DESCRIPTION
-    Not much to add. It's fairly simple.
-.PARAMETER CSVseparator
-   	Character to use for CSV separation.
-.PARAMETER CSVnoheader
-	Do not export header (column names) to CSV.
-.PARAMETER Header
-	Use custom header (NOT column names) in CSV.
-.PARAMETER Encoding
-    Specifies the type of character encoding used in the file. Valid values are "Unicode", "UTF7", "UTF8", "UTF32","ASCII", "BigEndianUnicode", "Default", and "OEM".
-.PARAMETER FileName
-	Name of target file fo export.
-.PARAMETER Xml
-	Export to XML instead of CSV.
-.NOTES
-    Author: Jack Olsson
-    Date:   2016-04-21
-}
-#>
-param (
-   [Parameter(Mandatory=$true,
-              ValueFromPipeline=$true,
-              ValueFromPipelineByPropertyName=$true)]
-   [System.Data.Datatable]$DataTable,
-   [Parameter(Mandatory=$true,              
-              ValueFromPipelineByPropertyName=$true)]
-   [string]$FileName,   
-   [string]$CSVseparator,
-   [switch]$CSVnoheader,
-   [string]$Header,
-   [string]$Encoding,
-   [switch]$xml
-   
-)
 
-Begin {
-}
-Process {
-   Write-Verbose $DataTable.TableName
-    if ($xml.IsPresent) {
-	    ($DataTable | ConvertTo-XML -NoTypeInformation).Save($FileName)	
-    } else {
-	    if ($CSVnoheader.IsPresent) {
-    		($DataTable | ConvertTo-Csv -Delimiter $CSVseparator -NoTypeInformation) -replace "`"", "" |  Select -Skip 1 | `
-	    		Out-File -Encoding $Encoding -Force $FileName
-	    } elseif (-not [string]::IsNullOrEmpty($Header)) {
-            $Header | Out-File -Encoding $Encoding -Force $FileName
-	    	($DataTable | ConvertTo-Csv -Delimiter $CSVseparator -NoTypeInformation) -replace "`"", "" |  Select -Skip 1 | `
-		    	Out-File -Encoding $Encoding -Append $FileName 
- 
-        } else {
-		    ($DataTable | ConvertTo-Csv -Delimiter $CSVseparator -NoTypeInformation) -replace "`"", "" | `
-			    Out-File -Encoding $Encoding -Force $FileName
-	    }
-    }
-}
-
-End {
-}
-}
 #endregion
 
 #region "Production" functions
@@ -196,6 +194,71 @@ function Msg {
             }
         }    
     }
+}
+
+function ExportDataTableToFile {
+<#
+.SYNOPSIS
+    Dump a datatable to CSV-file OR XML-file
+.DESCRIPTION
+    Not much to add. It's fairly simple.
+.PARAMETER CSVseparator
+   	Character to use for CSV separation.
+.PARAMETER CSVnoheader
+	Do not export header (column names) to CSV.
+.PARAMETER Header
+	Use custom header (NOT column names) in CSV.
+.PARAMETER Encoding
+    Specifies the type of character encoding used in the file. Valid values are "Unicode", "UTF7", "UTF8", "UTF32","ASCII", "BigEndianUnicode", "Default", and "OEM".
+.PARAMETER FileName
+	Name of target file fo export.
+.PARAMETER Xml
+	Export to XML instead of CSV.
+.NOTES
+    Author: Jack Olsson
+    Date:   2016-04-21
+}
+#>
+param (
+   [Parameter(Mandatory=$true,
+              ValueFromPipeline=$true,
+              ValueFromPipelineByPropertyName=$true)]
+   [System.Data.Datatable]$DataTable,
+   [Parameter(Mandatory=$true,              
+              ValueFromPipelineByPropertyName=$true)]
+   [string]$FileName,   
+   [string]$CSVseparator,
+   [switch]$CSVnoheader,
+   [string]$Header,
+   [string]$Encoding,
+   [switch]$xml
+   
+)
+
+Begin {
+}
+Process {
+   Write-Verbose $DataTable.TableName
+    if ($xml.IsPresent) {
+	    ($DataTable | ConvertTo-XML -NoTypeInformation).Save($FileName)	
+    } else {
+	    if ($CSVnoheader.IsPresent) {
+    		($DataTable | ConvertTo-Csv -Delimiter $CSVseparator -NoTypeInformation) -replace "`"", "" |  Select -Skip 1 | `
+	    		Out-File -Encoding $Encoding -Force $FileName
+	    } elseif (-not [string]::IsNullOrEmpty($Header)) {
+            $Header | Out-File -Encoding $Encoding -Force $FileName
+	    	($DataTable | ConvertTo-Csv -Delimiter $CSVseparator -NoTypeInformation) -replace "`"", "" |  Select -Skip 1 | `
+		    	Out-File -Encoding $Encoding -Append $FileName 
+ 
+        } else {
+		    ($DataTable | ConvertTo-Csv -Delimiter $CSVseparator -NoTypeInformation) -replace "`"", "" | `
+			    Out-File -Encoding $Encoding -Force $FileName
+	    }
+    }
+}
+
+End {
+}
 }
 
 # https://blogs.technet.microsoft.com/heyscriptingguy/2014/04/26/weekend-scripter-access-powershell-preference-variables/
